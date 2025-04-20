39e2d2aee9831a90f0f3871776740c1d63e3b03c
jakkie <jakubkielbasiewicz@gmail.com>
2025-04-21 01:23:44 +0200

    instruments

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 mod models;
 
-use models::{OkxResponse, Spread};
+use models::{Instrument, OkxResponse, Spread};
 use websocket::ClientBuilder;
 
-fn fetch_spreads() -> Vec<Spread> {
+fn fetch_instruments() -> Vec<Instrument> {
+    let mut instruments = vec![];
     let client = reqwest::blocking::Client::new();
-    let mut req = client.get("https://www.okx.com/api/v5/sprd/spreads");
+    let mut req = client.get("https://www.okx.com/api/v5/public/instruments");
 
+    for inst_type in ["SPOT", "SWAP"].iter() {
+        let req_with_query = req.try_clone().expect("Failed to clone request").query(&[("instType", inst_type)]);
 
-    let resp = req.send().unwrap();
-
-    if resp.status().is_success() {
-        let parsed: OkxResponse<Spread> = resp.json().unwrap();
-        parsed.data
-    } else {
-        eprintln!("❌ Failed to fetch spreads: {}", resp.status());
-        vec![]
+        let resp = req_with_query.send().unwrap();
+        if resp.status().is_success() {
+            let parsed: OkxResponse<Instrument> = resp.json().unwrap();
+            instruments.extend(parsed.data);
+        } else {
+            eprintln!("❌ Failed to fetch instruments: {}", resp.status());
+        }
     }
+    instruments
 }
 
 fn main() {
     let ws_url = "wss://ws.okx.com:8443/ws/v5/business";
 
-    let spreads = fetch_spreads();
+    let instruments = fetch_instruments();
 
-    println!("Spreads: {:?}", spreads);
+    println!("Spreads: {:?}", instruments);
+
+    println!("Length: {}", instruments.len());
 
 
 }
